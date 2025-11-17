aa00efb278785bb1e177dd36844d5d39050e97c6
karthikeyan <akarthikeyan9876@gmail.com>
2025-11-17 20:27:40 +0530

    Added CRUD functionalities

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,16 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/', views.add_Task, name="add_Task"),
+    
+    # Mark_as_done Feature
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name="mark_as_done"),
+    
+    # Mark_us_undone Feature
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name="mark_as_undone"),
+    
+    # Edit Feature
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    
+    # Delete Feature
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
